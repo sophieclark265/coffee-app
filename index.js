f4ccee27e6c5903f1a2791058b105f5c0650907b
Sophie <sophieclark526@gmail.com>
2022-10-26 20:46:57 -0700

    import express and axios to make request to kroger api to get acess token

--- index.js
+++ index.js
@@ -11,3 +11,39 @@ app.listen(2000, () => console.log("listening now!"));
 // give express a file to serve up
 
 app.use(express.static("client-files"));
+
+// AXIOS IMPORT
+
+let encoded = `c29waGllY2xhcmstMzJhODlmMzE4ZmNiZjAwYzAxZDI3ZmQwYmM0YjUxY2Y2NTYxMzk0MjM5MTg3NzYzOTk1Ok1ScHR1aWpqWnpzQ3BKU3ZVVmpsVTFoaWE1Vk5RTG1ZZlVHc1VFR18=`;
+
+let settings = {
+  async: true,
+  crossDomain: true,
+  url: "https://api.kroger.com/v1/connect/oauth2/token",
+  method: "POST",
+  headers: {
+    "Content-Type": "application/x-www-form-urlencoded",
+    Authorization: "Basic " + encoded,
+  },
+  data: {
+    grant_type: "client_credentials",
+    scope: "product.compact",
+  },
+};
+
+const axios = require("axios");
+
+app.get("/blah", async (req, res) => {
+  return res.send("Hello world");
+});
+
+const getAccessToken = async () => {
+  return axios
+    .request(settings)
+    .then(function (response) {
+      return response.data["access_token"];
+    })
+    .catch(function (error) {
+      throw error;
+    });
+};
