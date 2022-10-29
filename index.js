3f060f4c6fa6c003a96ac226f088aa7fc701107d
Sophie <sophieclark526@gmail.com>
2022-10-28 19:44:02 -0700

    cache access token in file in JSON format, and check if token expired on each call to kroger API. automatically refresh token if expired.

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const app = express();
 // call express
 
 // listen at a port
-app.listen(2000, () => console.log("listening now!"));
+app.listen(2000, () => console.log("okay server listening"));
 
 // give express a file to serve up
 
@@ -14,29 +14,55 @@ app.use(express.static("client-files"));
 
 // AXIOS IMPORT
 
-let baseEncoded = `c29waGllY2xhcmstMzJhODlmMzE4ZmNiZjAwYzAxZDI3ZmQwYmM0YjUxY2Y2NTYxMzk0MjM5MTg3NzYzOTk1Ok1ScHR1aWpqWnpzQ3BKU3ZVVmpsVTFoaWE1Vk5RTG1ZZlVHc1VFR18=`;
-
-let settings = {
-  async: true,
-  crossDomain: true,
-  url: "https://api.kroger.com/v1/connect/oauth2/token",
-  method: "POST",
-  headers: {
-    "Content-Type": "application/x-www-form-urlencoded",
-    Authorization: "Basic " + baseEncoded,
-  },
-  data: {
-    grant_type: "client_credentials",
-    scope: "product.compact",
-  },
-};
-
 const axios = require("axios");
 
+// nodejs file system library
+const fs = require("fs");
+
 const getAccessToken = async () => {
+  let tokenDetailsJSON = fs.readFileSync("./test", "utf-8");
+  let tokenDetails = JSON.parse(tokenDetailsJSON);
+
+  let timeNow = new Date().getTime();
+  if (timeNow < tokenDetails.expiry) {
+    console.log("not expired, returning from file");
+    return tokenDetails.token;
+  }
+
+  console.log("token expired, getting new token");
+  
+  let baseEncoded = `c29waGllY2xhcmstMzJhODlmMzE4ZmNiZjAwYzAxZDI3ZmQwYmM0YjUxY2Y2NTYxMzk0MjM5MTg3NzYzOTk1Ok1ScHR1aWpqWnpzQ3BKU3ZVVmpsVTFoaWE1Vk5RTG1ZZlVHc1VFR18=`;
+
+  let settings = {
+    async: true,
+    crossDomain: true,
+    url: "https://api.kroger.com/v1/connect/oauth2/token",
+    method: "POST",
+    headers: {
+      "Content-Type": "application/x-www-form-urlencoded",
+      Authorization: "Basic " + baseEncoded,
+    },
+    data: {
+      grant_type: "client_credentials",
+      scope: "product.compact",
+    },
+  };
+
+  // promise either resolves or rejects
   return axios
     .request(settings)
     .then(function (response) {
+      // console.log("access token is", response.data["access_token"]);
+      // get date time =
+      let time = new Date().getTime();
+      console.log("time", time);
+      let expiry = time + response.data["expires_in"] * 1000;
+
+      let tokenDetails = JSON.stringify({
+        expiry: expiry,
+        token: response.data["access_token"],
+      });
+      fs.writeFileSync("./test", tokenDetails);
       return response.data["access_token"];
     })
     .catch(function (error) {
@@ -44,35 +70,42 @@ const getAccessToken = async () => {
     });
 };
 
-getAccessToken();
-
-const accessToken = `eyJhbGciOiJSUzI1NiIsImprdSI6Imh0dHBzOi8vYXBpLmtyb2dlci5jb20vdjEvLndlbGwta25vd24vandrcy5qc29uIiwia2lkIjoiWjRGZDNtc2tJSDg4aXJ0N0xCNWM2Zz09IiwidHlwIjoiSldUIn0.eyJhdWQiOiJzb3BoaWVjbGFyay0zMmE4OWYzMThmY2JmMDBjMDFkMjdmZDBiYzRiNTFjZjY1NjEzOTQyMzkxODc3NjM5OTUiLCJleHAiOjE2NjY4OTcxMjIsImlhdCI6MTY2Njg5NTMxNywiaXNzIjoiYXBpLmtyb2dlci5jb20iLCJzdWIiOiJhY2UwOTMxNC1hZGJkLTVlMzUtYmI0YS1jZTQzZTExNGNiYmMiLCJzY29wZSI6InByb2R1Y3QuY29tcGFjdCIsImF1dGhBdCI6MTY2Njg5NTMyMjkyMjkzMzI3NiwiYXpwIjoic29waGllY2xhcmstMzJhODlmMzE4ZmNiZjAwYzAxZDI3ZmQwYmM0YjUxY2Y2NTYxMzk0MjM5MTg3NzYzOTk1In0.DBUD9tX1OyATU9kPIegxC65tUwSBB3bzVWODJW0dY4fQyUJaeGJBK3VvIsAA_n-ZHUwSB1sPsuwtjvNSTYJeswGcNCx6nSC79-T6iP1R94Y_1TuY9oI6duB4QlG-kpHnmgdsBaPDxQt1zINrv6m7QDa6wJ12igGD-WuGa2TvTxiTCub9JI8yuZUbQcKoYyj9LlccvDvvoPRp5MN32Oj6Z8EZKMQvZlmtNnIbzAa14Z55pTzVzh1Lwn6Zr8-jm2TPv4wjalsSeyy4FLb_NFyoLTrYdjenw4ZH7GjUlmYYA4gcLew3NTzCY5hU_F08l4QDLnzG7QXjjP7QyVuqampYZg`;
-let term = "milk";
-let limit = 2;
-var options = {
-  async: true,
-  crossDomain: true,
-  url: `https://api.kroger.com/v1/products?filter.term=${term}&filter.limit=${limit}`,
-  method: "GET",
-  headers: {
-    Accept: "application/json",
-    Authorization: `Bearer ${accessToken}`,
-  },
-};
-
 const callKroger = async () => {
+  let term = "milk";
+  let limit = 2;
+  const accessToken = await getAccessToken();
+
+  var options = {
+    async: true,
+    crossDomain: true,
+    url: `https://api.kroger.com/v1/products?filter.term=${term}&filter.limit=${limit}`,
+    method: "GET",
+    headers: {
+      Accept: "application/json",
+      Authorization: `Bearer ${accessToken}`,
+    },
+  };
+
   return axios
     .request(options)
     .then(function (response) {
       console.log("successfully called kroger");
-      console.log("response is", response.data);
+      // console.log("response is", response.data);
+      return response.data;
     })
     .catch(function (error) {
+      // potential 401 http status code (i.e. unauthorized)
       console.log("error");
       throw error;
     });
 };
 
-callKroger();
+const main = async () => {
+  console.log(await callKroger());
+  const fileContents = fs.readFileSync("./test", "utf-8");
+  console.log("fileContents", fileContents);
+};
+
+main();
 
 // need to figure out how to dynamically update Access Token var from getAccessToken (as is, when i create key and assign response.data[access-token] to it i get undefined when passing into next axios.get request)
